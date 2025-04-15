352576e318a6411d7373f2fafc2e2212fb7e58b2
Bennett Upfield <bjupfield@gmail.com>
2025-04-15 18:09:34 -0500

    memory manager worked first try, wow

--- swcWindow.c
+++ swcWindow.c
@@ -63,7 +63,7 @@ swcArena creArena(size_t size, size_t minData)
  * @return uint32_t 
  */
 
-uint32_t desArena(swcArena *arena)
+uint32_t freeArena(swcArena *arena)
 {
     //TODO: add decoupling from garbage collector
     free(arena->origin);
@@ -105,7 +105,8 @@ void *alloc(swcArena *a, size_t size)
 /**
  * @brief Create a memory manager with initial size of stSize
  * 
- * @param stSize 
+ * @param stSize Desired Arena Count
+ * @param count Names Initialization
  * @return swcMemMan 
  */
 swcMemMan createMan(uint32_t stSize, uint32_t count)
@@ -118,11 +119,12 @@ swcMemMan createMan(uint32_t stSize, uint32_t count)
         .nameSize = count,
         .nameCount = 1,//tree start
     };
-    swcArena *a = addArena(sizeof(swcName) * 2000, sizeof(swcName), &ret);
+    swcArena *a = addArena((sizeof(swcName) + (-sizeof(swcName) & alignof(swcName))) * count, sizeof(swcName), &ret);
+    //TODO: change maxsize to something dynamic
 
     ret.namesTree = (swcName*)alloc(a, sizeof(swcName));
     ret.namesTree->name = 1 << 15;//for binary tree start, max divs i guess is sizeof(uint32_t) - 1
-    ret.namesTree->pointer = 0;
+    ret.namesTree->pointer = NULL;
     ret.namesTree->size = 0;
     ret.namesTree->lSize = 0;
     ret.namesTree->rSize = 0;
@@ -130,7 +132,7 @@ swcMemMan createMan(uint32_t stSize, uint32_t count)
 }
 
 /**
- * @brief Add an arena to the manager
+ * @brief Add an arena to the manager returns null if would exceed arena count
  * 
  * @param size 
  * @param avgData 
@@ -141,10 +143,11 @@ swcArena* addArena(size_t size, size_t avgData, swcMemMan* manager)
 {
     if(manager->count < manager->size)
     {
-        manager->count += 1; 
-        manager->arenas[manager->count - 1] = creArena(size, avgData);
+        manager->arenas[manager->count] = creArena(size, avgData);
+        manager->count++; 
+        return &(manager->arenas[manager->count - 1]);
     }
-    //TODO: expand arena container and create another
+    return NULL;
 }
 
 /**
@@ -152,7 +155,7 @@ swcArena* addArena(size_t size, size_t avgData, swcMemMan* manager)
  * 
  * @param size 
  * @param manager 
- * @return void* 
+ * @return void* || Returns allocated if success if fail returns NULL
  */
 void *allocM(size_t size, swcMemMan *manager)
 {
@@ -182,6 +185,7 @@ uint32_t allocNamed(size_t size, swcMemMan* manager)
     uint32_t genName = root->name;
     while(root->lChild == 0 && root->rChild == 0)
     {
+        genName >>= 1;
         if(root->lSize > root->rSize)
         {
             root->rSize++;
@@ -191,7 +195,6 @@ uint32_t allocNamed(size_t size, swcMemMan* manager)
         }
         root->lSize++;
         root = root->lChild;
-        genName >>= 1;
     }
     if(root->lSize > root->rSize)
     {
@@ -211,27 +214,77 @@ uint32_t allocNamed(size_t size, swcMemMan* manager)
     name->par = root;
     name->size = size;
 
+    manager->nameCount++;
+
     return genName;
 }
 
 /**
+ * @brief Sets a names size to 0, 
+ * cannot set a names name to zero as to maintain a binary tree a name cannot be set to 0
+ * otherwise its unsortable
+ * 
+ * @param name 
+ * @return uint32_t dealloced name if succes, 0 if failure
+ */
+uint32_t deallocNamed(uint32_t name, swcMemMan* manager)
+{
+    swcName* root = manager->namesTree;
+    while(root != NULL && root->name != name)
+    {
+        if(name > root->name)
+        {
+            root = root->rChild;
+            continue;
+        }
+        root = root->lChild;
+    }
+    if(root == NULL)
+    {
+        //TODO:
+        //this either means a fake name is given or it has failed
+        return 0;
+    }
+    root->size = 0;
+    return name;
+}
+
+/**
+ * @brief frees a manager passed as a pointer
+ * 
+ * @param manager 
+ * @return uint32_t 
+ */
+uint32_t freeMemMan(swcMemMan* manager)
+{
+    for(int i = 0; i < manager->count; i++)
+    {
+        freeArena(&manager->arenas[i]);
+    }
+    free(manager->arenas);
+    return 1;
+}
+
+/**
  * @brief Reorganizes Arenas, returns new "manager"
+ * only Named data blocks are copied over to the new manager, all else are scrapped... 
+ * passed manager will be made null
  * 
  * @param manager
  * @return uint32_t 
  */
 swcMemMan reconfigureArenas(swcMemMan* manager)
 {
-    uint32_t avgSize = 0;
+    uint32_t avgSize = 2000;//=>for initial pointer array fo rbinary tree copying
     uint32_t minSize = 0;
     uint32_t i;
     for(i = 0; i < manager->count; i++)
     {
         avgSize += manager->arenas[i].size; 
-        minSize = minSize < manager->arenas[i].mData ? manager->arenas[i].mData : minSize;
+        minSize = minSize < manager->arenas[i].minData ? manager->arenas[i].minData : minSize;
     }
     avgSize /= i;
-    avgSize = minSize < avgSize ? avgData : minSize;
+    avgSize = minSize < avgSize ? avgSize : minSize;
 
     /*
     * This might be the completely incorrect approach, it might be better to just configure all arena
@@ -239,24 +292,80 @@ swcMemMan reconfigureArenas(swcMemMan* manager)
     * or how long term the data is for now ill pretend that this is the faster method
     */
 
-    swcMemMan ret = createMan(manager->size * 1.5);//sure 1.5, hope there is enough discarded data to justify the small increase, hopefully there is a decrease of total data
+    swcMemMan ret = createMan(manager->size * 1.5, manager->nameCount * 1.5);//sure 1.5, hope there is enough discarded data to justify the small increase, hopefully there is a decrease of total data
 
-    swcArena *curArena = addArena(avgSize, avgData, &ret);
-    for(i = 0; i < manager->count; i++)
+    swcArena *curArena = addArena(avgSize, minSize, &ret);//this line is a problem i dont know how to fix it
+    //TODO: FIX ABOVE LINE(MINSIZE and AVGSIZE conflict, or not really, but it creates a memory size problem where every arena is made for the largest arena)
+    //i guess its not really a problem now that I think of it... but whatever
+
+    swcName* root = manager->namesTree;
+    swcName* parent = ret.namesTree;
+    swcName* lChilds = allocM(sizeof(lChilds) * 24, &ret);//only need 16 lchilds to fully copy 2^32
+    uint32_t lChildsCount = 0;
+    if(root->lChild != NULL)
     {
-        manager->arenas[i].end -= sizeof(size_t);
-        while(manager->arenas->end >= manager->arenas->assignedPointers)
-        {
-            void* data = (void*)*(manager->arenas->end);
-            size_t size = *(manager->arenas->end + sizeof(size_t));
-            void* buffer = alloc(curArena, size);
-            alloc(curArena, size);
-            memcpy()
-
-            manager->arenas->end -= sizeof(swcAssignedPointer);
-        }
-
+        *lChilds = *root->lChild;
+        lChildsCount++;
     }
+    root = root->rChild;
+    while(1)
+    {
+        //actually change storage here
+        if(root->size != 0)
+        {
+            swcName* curName = (swcName*)alloc(manager->arenas, sizeof(swcName));
+            curName->name = root->name;
+            curName->size = root->size;
+            curName->par = parent;
+
+            curName->pointer = allocM(curName->size, &ret);
+            if(curName->pointer == NULL)
+            {
+                if(addArena(avgSize, minSize, &ret) == NULL)
+                {
+                    //TODO: explode, just probably reiterate reorganize request with some sort of scalar to make sure the arenas will cover the size
+                }
+                curName->pointer = allocM(curName->size, &ret);
+                if(curName->pointer == NULL)
+                {
+                    //TODO: this shouldnt occur, if it does ive mismanaged the memory space
+                }
+
+            }
+            memcpy(curName->pointer, root->pointer, curName->size);
+
+            if(parent->name > curName->name)
+            {
+                parent->lChild = curName;
+            }
+            else
+            {
+                parent->rChild = curName;
+            }
+            parent = curName;
+            ret.nameCount++;
+        }
+        if(root->rChild != NULL)
+        {
+            lChildsCount++;
+            *(lChilds + lChildsCount) = *root->lChild;//check in lchildsloop to see if its null
+
+            root = root->rChild;
+            continue;
+        }
+        if(lChildsCount >= 0)
+        {
+            if((lChilds + lChildsCount) != NULL)
+            {
+                root = (lChilds + lChildsCount);
+                lChildsCount--;
+            }
+            continue;
+        }
+        break;
+    }
+    freeMemMan(manager);
+    return ret;
 }
 
 
@@ -321,122 +430,29 @@ swcWin initWindow(uint32_t* config, uint64_t eventMask, uint32_t posx, uint32_t 
     //remove dummy
     XDestroyWindow(display, win.mainWin);
 
-    swcArena a;
-    ;
+    win.manager = createMan(sizeof(swcDiv) * 5000, 10);
+    freeMemMan(&win.manager);
 
     return win;
 }
-// struct theWindow mainWin;
-// int initializeWindow()
-// {
-//     struct theWindow mainWin = 
-//     {
-//         .divs = (winDiv*)malloc(sizeof(winDiv) * 512),
-//         .divArrSize = 512,
-//         .divCount = 0,
-//     };
-//     return 1;
-// }
-// int adjustWinSize()
-// {
-//     return 0;
-// }
-// //lazy right now
-// int addDiv(int posx, int posy, int width, int height)
-// {
-//     //shouldnt ever happen
-//     if(mainWin.divCount >= mainWin.divArrSize)
-//     {
-//         mainWin.divArrSize *= 2;
-//         mainWin.divs = (winDiv*)realloc(mainWin.divs, sizeof(winDiv) * mainWin.divArrSize);
-//     }
+/**
+ * @brief 
+ * 
+ * @param win 
+ * @param parent 
+ * @param posx 
+ * @param posy 
+ * @param dimx 
+ * @param dimy 
+ * @param onLoad 
+ * @param drawFunc 
+ * @param resizeFunc 
+ * @param eventFunc 
+ * @return swcDiv* 
+ */
+swcDiv* initDiv(swcWin* win, swcDiv* parent, uint32_t posx, uint32_t posy, 
+    uint32_t dimx, uint32_t dimy, funcPointer onLoad, funcPointer drawFunc,
+    resizePointer resizeFunc, handlePointer eventFunc)
+{
 
-//     winDiv* init = &mainWin.divs[mainWin.divCount];
-//     init->height = height;
-//     init->width = width;
-//     init->xPos = posx;
-//     init->yPos = posy;
-
-//     mainWin.divCount += 1;
-
-//     return 1;
-// }
-// vertexBI drawWindow()
-// {
-//     struct corner
-//     {
-//         int x;
-//         int y;
-//     };
-//     typedef struct corner corn;
-//     vertexBI ret;
-//     ret.elements = 6;
-//     ret.vertexCount = mainWin.divCount * 6;
-//     ret.vertexBuffer = mainWin.vBuffer;//hmmm
-//     //TODO:
-//     // to make this like analyze what actually needs to be adjusted, like flag vertexses that are wrong... this is complicated so we push it off
-
-//     ret.vertexBuffer = (float*)malloc(sizeof(float) * ret.elements * ret.vertexCount);
-
-//     float c1[3] = {1.0, 0.0, 0.0};
-//     float c2[3] = {0.0, 1.0, 0.0};
-//     float c3[3] = {0.0, 0.0, 1.0};
-
-//     for(int i = 0; i < ret.vertexCount; i++)
-//     {
-//         winDiv drawDiv = mainWin.divs[i];  
-//         corn leftU = 
-//         {
-//             .x = drawDiv.xPos,
-//             .y = drawDiv.yPos
-//         };
-//         corn rightU = 
-//         {
-//             .y = leftU.y,
-//             .x = drawDiv.width + leftU.x,
-//         };
-//         corn leftL = 
-//         {
-//             .y = leftU.y + drawDiv.height,
-//             .x = leftU.x,
-//         };
-//         corn rightL = 
-//         {
-//             .y = leftL.y,
-//             .x = rightU.x,
-//         };
-
-//         //ll corner t1
-//         float horror[3] = {leftL.x, leftL.y, 0.0 };
-//         memcpy((float*)ret.vertexBuffer + i * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + i * ret.elements + 3, c1, 12);
-
-//         //ul corner t1
-//         float horror[3] = {leftU.x, leftU.y, 0.0};
-//         memcpy((float*)ret.vertexBuffer + (i + 1) * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + (i + 1) * ret.elements + 3, c2, 12);
-
-//         //ur corner t1
-//         float horror[3] = {rightU.x, rightU.y, 0.0};
-//         memcpy((float*)ret.vertexBuffer + (i + 2) * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + (i + 2) * ret.elements + 3, c3, 12);
-
-//         //ll corner t2
-//         float horror[3] = {leftL.x, leftL.y, 0.0};
-//         memcpy((float*)ret.vertexBuffer + (i + 3) * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + (i + 3) * ret.elements + 3, c1, 12);
-
-//         //lr corner t2
-//         float horror[3] = {rightL.x, rightL.y, 0.0};
-//         memcpy((float*)ret.vertexBuffer + (i + 4) * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + (i + 4) * ret.elements + 3, c2, 12);
-
-//         //ur corner t2
-//         float horror[3] = {rightU.x, rightU.y, 0.0};
-//         memcpy((float*)ret.vertexBuffer + (i + 5) * ret.elements, horror, 12);
-//         memcpy((float*)ret.vertexBuffer + (i + 5) * ret.elements + 3, c3, 12);
-//         //i need to plan this...
-//     }
-
-//     return ret;
-// }
+}
--- swcWindow.h
+++ swcWindow.h
@@ -15,11 +15,74 @@
 extern uint32_t defConfiguration[];
 extern uint64_t defMask;
 
+/*
+* Arena Definition
+* This arena is specifically designed for divs
+* and will allow for the freeing of data
+*/
+
+typedef struct {
+    char* beg;//current next allocation location
+    char* end;//current last available memory for normal allocation(also the start of assignedpointers)
+    char* origin;//actual pointer to beginning of memory location of arena//  every bit of data stored gains an assignedpointer datapointer
+//  which eventually we will program a "garbage collector"
+//  which when the arena is full will begin a reallocation process where a new arena is allocated, or several
+//  whic are than filled with only assigned pointers, which can be deleted with a dealloc function, dereferncing data
+//  so that it will not be saved once the new arena is allocated
+    uint32_t size;
+    uint32_t minData;
+}swcArena;
+
+
+swcArena creArena(size_t size, size_t minData);
+uint32_t freeArena(swcArena *arena);
+void *alloc(swcArena *a, size_t size);
+
+/**
+ * @brief Name Pointer Size contianer
+ * 
+ */
+typedef struct swcName {
+    uint32_t name;
+    void* pointer;
+    size_t size;
+    struct swcName* lChild;
+    uint32_t lSize;
+    struct swcName* rChild;
+    uint32_t rSize;
+    struct swcName* par;
+}swcName;
+
+
+
+/**
+ * @brief Memory Manager 
+ * 
+ */
+typedef struct {
+    uint32_t count;
+    uint32_t size;
+    swcArena* arenas;
+    uint32_t nameCount;
+    uint32_t nameSize;
+    swcName* namesTree;
+}swcMemMan;
+
+//TODO:
+// Add single frame and double frame arenas for smaller data dynamic data within the divs
+
+swcMemMan createMan(uint32_t stSize, uint32_t count);
+void* allocM(size_t size, swcMemMan* manager);
+uint32_t allocNamed(size_t size, swcMemMan *manager);
+uint32_t deallocNamed(uint32_t name, swcMemMan *manager);
+swcArena* addArena(size_t size, size_t minData, swcMemMan *manager);
+
 typedef struct {
     Window mainWin;
     uint32_t programGroupCount;
     uint32_t* groupCounts;
     uint32_t handleGroupCount;
+    swcMemMan manager;
 
     /*
     * Okay What we have run into is we need mass allocation of dynamic memory
@@ -39,66 +102,6 @@ swcWin initWindow(uint32_t* config, uint64_t eventMask, uint32_t posx, uint32_t 
 // int addDiv();
 // vertexBI drawWindow();
 
-/*
-* Arena Definition
-* This arena is specifically designed for divs
-* the alignment will be max alignment... just because, why not, whats the worst that could happen, at most we lose 8 bytes 
-* per padding, but storing if a div needs more alignment would cost more
-* Assigned pointers will be stored at the end with backwards linking, 
-* so the amount of space used will equivalent to requestedallocation / minSize * sizeof(swcAssignedPointer)
-* and will allow for the freeing of data
-*/
-
-typedef struct {
-    char* beg;//current next allocation location
-    char* end;//current last available memory for normal allocation(also the start of assignedpointers)
-    char* origin;//actual pointer to beginning of memory location of arena//  every bit of data stored gains an assignedpointer datapointer
-//  which eventually we will program a "garbage collector"
-//  which when the arena is full will begin a reallocation process where a new arena is allocated, or several
-//  whic are than filled with only assigned pointers, which can be deleted with a dealloc function, dereferncing data
-//  so that it will not be saved once the new arena is allocated
-    uint32_t size;
-    uint32_t minData;
-}swcArena;
-
-
-swcArena creArena(size_t size, size_t minData);
-uint32_t desArena(swcArena *arena);
-void *alloc(swcArena *a, size_t size);
-
-/**
- * @brief Name Pointer Size contianer
- * 
- */
-typedef struct {
-    uint32_t name;
-    size_t pointer;
-    size_t size;
-    swcName* lChild;
-    uint32_t lSize;
-    swcName* rChild;
-    uint32_t rSize;
-    swcName* par;
-}swcName;
-
-/**
- * @brief Memory Manager 
- * 
- */
-typedef struct {
-    uint32_t count;
-    uint32_t size;
-    swcArena* arenas;
-    uint32_t nameCount;
-    uint32_t nameSize;
-    swcName* namesTree;
-}swcMemMan;
-
-swcMemMan createMan(uint32_t stSize, uint32_t count);
-void* allocM(size_t size, swcMemMan* manager);
-uint32_t allocNamed(size_t size, swcMemMan *manager);
-uint32_t deallocNamed(uint32_t name);
-swcArena* addArena(size_t size, size_t minData, swcMemMan *manager);
 
 
 struct swcDiv;
@@ -142,5 +145,9 @@ typedef struct {
     * at least a default constructor can be provided, that you just need to pass sizeof(yourdiv)
     */
 }swcDiv;
+
+swcDiv* initDiv(swcWin* win, swcDiv* parent, uint32_t posx, uint32_t posy, 
+    uint32_t dimx, uint32_t dimy, funcPointer onLoad, funcPointer drawFunc,
+    resizePointer resizeFunc, handlePointer eventFunc);
 
 
